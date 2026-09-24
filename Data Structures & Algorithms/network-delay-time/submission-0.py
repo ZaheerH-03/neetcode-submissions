71a1310678174a4ee90917c25a15bc3868a8b16a
Zaheer Hussain <zaheerhussain160304@gmail.com>
2026-09-24 18:28:17 +0530

    Add: network-delay-time - submission-0

---
+++ Data Structures & Algorithms/network-delay-time/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        edges = collections.defaultdict(list)
+        for u,v,t in times:
+            edges[u].append((v,t))
+
+        minHeap = [(0,k)]
+        visit = set()
+        time = 0
+        while minHeap:
+            t1 , v = heapq.heappop(minHeap)
+            if v in visit:
+                continue
+            visit.add(v)
+            time = t1
+            for v2,t2 in edges[v]:
+                if v2 not in visit:
+                    heapq.heappush(minHeap,(t1+t2,v2))
+        return time if len(visit)==n else -1
